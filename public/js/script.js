a5880b60befb8b16847bb774e8ec800dee005190
Darylabrador <abrador.daryl@gmail.com>
2021-06-14 19:12:22 +0400

    condition to invalid folder that's not contain .git folder

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ import FileTree from '../../utils/FileTree.js';
 const remote = require('electron').remote;
 const { dialog } = remote;
 const path = require('path');
+const fs = require('fs');
 
 window.addEventListener('DOMContentLoaded', (event) => {
     let req = new XMLHttpRequest();
@@ -9,35 +10,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     let loading = document.getElementById('loading');
     let directoryNameDisplay = document.getElementById("directoryNameDisplay");
+    let invalideFolder = document.getElementById("invalideFolder");
     let openFolder = document.getElementById("openFolder");
 
     const setDirectoryName = (directoryName) => {
         directoryNameDisplay.textContent = directoryName;
     }
 
-    openFolder.addEventListener("click",  async (evt) => {
+    openFolder.addEventListener("click", async (evt) => {
         try {
             loading.classList.remove('d-none');
+            invalideFolder.classList.add('d-none');
             let directory = await dialog.showOpenDialog({ properties: ['openDirectory'] });
 
-            if(directory.canceled){
+            if (directory.canceled) {
                 loading.classList.add('d-none');
             } else {
+                if (directory.filePaths && directory.filePaths.length != 0) {
 
-                if(directory.filePaths && directory.filePaths.length != 0){
-                    let directoryArray = directory.filePaths[0].split("\\");
-                    let directoryName  = directoryArray[directoryArray.length - 1];
-                    await setDirectoryName(directoryName);
-    
-                    var fileTree = new FileTree(directory.filePaths[0]);
-                    fileTree.build();
-                    console.log(fileTree);
-                    loading.classList.add('d-none');
+                    fs.access(path.join(directory.filePaths[0], ".git"), async function (error) {
+                        if (error) {
+                            invalideFolder.classList.remove('d-none')
+                        } else {
+                            let directoryArray = directory.filePaths[0].split("\\");
+                            let directoryName = directoryArray[directoryArray.length - 1];
+                            await setDirectoryName(directoryName);
+
+                            var fileTree = new FileTree(directory.filePaths[0]);
+                            fileTree.build();
+                            console.log(fileTree);
+                            loading.classList.add('d-none');
+                            invalideFolder.classList.add('d-none');
+                        }
+                    })
                 }
             }
         } catch (error) {
             console.log(error)
         }
- 
+
     })
 });
