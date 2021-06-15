44077a38b0815aa2440033de7d9957fc885bd2bb
Darylabrador <abrador.daryl@gmail.com>
2021-06-15 11:25:41 +0400

    open first lvl folder

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
-import FileTree from '../../utils/FileTree.js';
 const remote = require('electron').remote;
 const { dialog } = remote;
 const path = require('path');
 const fs = require('fs');
+const dirTree = require("directory-tree");
 
 window.addEventListener('DOMContentLoaded', (event) => {
     let req = new XMLHttpRequest();
@@ -12,35 +12,108 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let directoryNameDisplay = document.getElementById("directoryNameDisplay");
     let invalideFolder = document.getElementById("invalideFolder");
     let openFolder = document.getElementById("openFolder");
+    let folderContent = document.getElementById('folderContent');
+    let fileViewer = document.getElementById('fileViewer');
 
     const setDirectoryName = (directoryName) => {
         directoryNameDisplay.textContent = directoryName;
     }
 
+    function test() {
+        console.log('tu me fait chier')
+    }
+
     openFolder.addEventListener("click", async (evt) => {
         try {
+            folderContent.innerHTML = "";
+            directoryNameDisplay.textContent = "Choisir le projet git";
             loading.classList.remove('d-none');
             invalideFolder.classList.add('d-none');
             let directory = await dialog.showOpenDialog({ properties: ['openDirectory'] });
 
             if (directory.canceled) {
                 loading.classList.add('d-none');
+                directoryNameDisplay.textContent = "Choisir le projet git";
+                folderContent.innerHTML = "";
             } else {
                 if (directory.filePaths && directory.filePaths.length != 0) {
 
                     fs.access(path.join(directory.filePaths[0], ".git"), async function (error) {
                         if (error) {
-                            invalideFolder.classList.remove('d-none')
+                            folderContent.innerHTML = "";
+                            invalideFolder.classList.remove('d-none');
+                            directoryNameDisplay.textContent = "Choisir le projet git";
                         } else {
-                            let directoryArray = directory.filePaths[0].split("\\");
-                            let directoryName = directoryArray[directoryArray.length - 1];
-                            await setDirectoryName(directoryName);
-
-                            var fileTree = new FileTree(directory.filePaths[0]);
-                            fileTree.build();
-                            console.log(fileTree);
                             loading.classList.add('d-none');
                             invalideFolder.classList.add('d-none');
+                            const filteredTree = dirTree(directory.filePaths[0]);
+
+                            if (filteredTree) {
+                                setDirectoryName(filteredTree.name);
+                                if (filteredTree.children) {
+                                    let renderHtml = "";
+
+                                    filteredTree.children.forEach(element => {
+                                        if (element.type == "directory") {
+                                            renderHtml += `
+                                            <button class="btn text-white d-flex align-items-center fw-bold btnInfoPath" data-path="${element.path}">
+                                                <span class="iconify" data-inline="false" data-icon="ant-design:folder-open-filled" style="color: #fff; font-size: 19px; margin-right: 5px;"></span>
+                                                <span> ${element.name} </span>
+                                            </button>
+                                            <div class="subContent"></div>`;
+                                        } else if (element.type == "file") {
+                                            renderHtml += `
+                                            <button class="btn text-white d-flex align-items-center btnInfoPath" data-path="${element.path}">
+                                                <span> ${element.name} </span>
+                                            </button>
+                                            <div class="subContent"></div>`;
+                                        }
+                                    });
+
+                                    folderContent.innerHTML = renderHtml;
+                                    let btnInfoPath = document.querySelectorAll('.btnInfoPath');
+                                    let subContent = document.querySelectorAll('.subContent');
+
+                                    btnInfoPath.forEach(btn => {
+                                        btn.addEventListener('click', evt => {
+                                            subContent.forEach(item => {
+                                                item.innerHTML = "";
+                                            });
+
+                                            if(btn.classList.contains('active')){
+                                                btn.classList.remove('active');
+                                                renderHtml = "";
+                                                btn.nextElementSibling.innerHTML = renderHtml;
+                                            } else {
+                                                btn.classList.add('active');
+                                                let contentTree = dirTree(btn.getAttribute('data-path'));
+                                                renderHtml = "";
+                                                contentTree.children.forEach(element => {
+                                                    if (element.type == "directory") {
+                                                        renderHtml += `
+                                                            <button class="btn text-white d-flex align-items-center fw-bold btnInfoPath" data-path="${element.path}">
+                                                                <span class="mx-2"> - </span>
+                                                                <span class="iconify" data-inline="false" data-icon="ant-design:folder-open-filled" style="color: #fff; font-size: 19px; margin-right: 5px;"></span>
+                                                                <span> ${element.name} </span>
+                                                            </button>
+                                                            <div class="subContent"></div>`;
+                                                    } else if (element.type == "file") {
+                                                        renderHtml += `
+                                                            <button class="btn text-white d-flex align-items-center btnInfoPath" data-path="${element.path}">
+                                                                <span class="mx-2"> - </span>
+                                                                <span> ${element.name} </span>
+                                                            </button>
+                                                            <div class="subContent"></div>`;
+                                                    }
+                                                });
+                                                btn.nextElementSibling.innerHTML = renderHtml;
+                                            }
+                                        })
+                                    })
+                                }
+
+                                console.log(filteredTree)
+                            }
                         }
                     })
                 }
@@ -48,6 +121,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
         } catch (error) {
             console.log(error)
         }
-
     })
 });
