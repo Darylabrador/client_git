e5b6669513fdd568fef3cccd764c977d6c111b80
Darylabrador <abrador.daryl@gmail.com>
2021-06-18 15:19:47 +0400

    update

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var generalController = require('../controllers/generalController');
-
-/* GET home page. */
-router.get('/', generalController.getIndex);
-
-module.exports = router;
